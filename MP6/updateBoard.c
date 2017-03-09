fe5932dabf794c31d88e3483a962835af4b410e7
rzhang56 <rzhang56@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-03-09 02:47:07 +0000

    aliveStable remaining

--- MP6/updateBoard.c
+++ MP6/updateBoard.c
@@ -12,7 +12,54 @@
  * Pay attention for the edge and corner cells, they have less neighbors.
  */
 
-int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
+int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col)
+{
+	int boardRowSize = r;
+	int boardColSize = c;
+	if (row == 0)
+	{
+		if (col == 0)						// top left corner
+		{
+			return(isAlive(r,c, board, 0, 1) + isAlive(r,c, board, row+1 , 0) + isAlive(r,c, board, row+1, col+1));
+		} else if (col == boardColSize)		// top right corner
+		{
+			return(isAlive(r,c, board, 0, col-1) + isAlive(r,c, board, row+1 , col) + isAlive(r,c, board, row-1, col-1));
+		}else 								// top row not corner
+		{
+			return(isAlive(r,c, board, 0, col-1) + isAlive(r,c, board, 0, col+1) + 
+				isAlive(r,c, board, 1, col-1) + isAlive(r,c, board, 1, col+1) +isAlive(r,c, board, 1 , col));
+		}
+	}
+	if (row == boardRowSize)
+	{
+		if (col == 0)						// bottom left corner
+		{
+			return(isAlive(r,c, board, row-1, 0) + isAlive(r,c, board, row-1 , 1) + isAlive(r,c, board, row, 1));
+		} else if (col == boardColSize)		// bottom right corner
+		{
+			return(isAlive(r,c, board, row-1, col) + isAlive(r,c, board, row-1 , col-1) + isAlive(r,c, board, row, col-1));
+		}else 								// bottom row not corner
+		{
+			return(isAlive(r,c, board, row, col-1) + isAlive(r,c, board, row, col+1) + 
+				isAlive(r,c, board, row-1, col-1) + isAlive(r,c, board, row-1, col+1) +isAlive(r,c, board, row-1 , col));
+		}
+
+	}
+
+	if (col == 0)							// left colomn not corner
+	{
+		return(isAlove(r,c, board, row-1, col) + isAlove(r,c, board, row+1, col) + 
+			isAlove(r,c, board, row-1, col+1) + isAlove(r,c, board, row, col+1) + isAlove(r,c, board, row+1, col+1));
+	} else if (col == boardColSize)
+	{
+		return(isAlove(r,c, board, row-1, col) + isAlove(r,c, board, row+1, col) + 
+			isAlove(r,c, board, row-1, col-1) + isAlove(r,c, board, row, col-1) + isAlove(r,c, board, row+1, col-1));
+	}
+	// not edge case
+	return(isAlive(r,c, board, row-1, col-1) + isAlive(r,c, board, row-1, col) + isAlive(r,c, board, row-1, col+1) + 
+		   isAlive(r,c, board, row, col-1)   + 							   isAlive(r,c, board, row, col+1) + 
+		   isAlive(r,c, board, row+1, col-1) + isAlive(r,c, board, row+1, col) + isAlive(r,c, board, row+1, col+1));									
+	
 }
 /*
  * Update the game board to the next step.
@@ -24,6 +71,31 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+	int i, len, curr;
+	int r, c;
+	len = boardRowSize * boardColSize;
+	for (i = 0; i < len; i++)
+	{
+		r = i / boardRowSize;
+		c = i % boardColSize;
+		curr = board[i];
+		switch(curr)
+		{
+			case 0:
+				if(countLiveNeighbor(board, boardRowSize, boardColSize, r, c) == 3)
+				{
+					board[i] = 1;
+					break;
+				}
+			case 1:
+				if(countLiveNeighbor(board, boardRowSize, boardColSize, r, c) != 2 ||
+					countLiveNeighbor(board, boardRowSize, boardColSize, r, c) != 3)
+				{
+					board[i] = 0;
+					break;
+				}
+		}
+	}
 }
 
 /*
@@ -37,10 +109,13 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * current step or there is no alive cells at all.
  * return 0 if the alive cells change for the next step.
  */ 
-int aliveStable(int* board, int boardRowSize, int boardColSize){
+int aliveStable(int b, int c, int* board, int boardRowSize, int boardColSize){
 }
 
-				
+int isAlive(int r, int c, int* board, int row, int col)
+{
+	return board(row*c + col);
+}				
 				
 			
 
--- MP6/updateBoard.h
+++ MP6/updateBoard.h
@@ -1,3 +1,4 @@
 void updateBoard(int* board, int boardRowSize, int boardColSize);
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col);
 int aliveStable(int* board, int boardRowSize, int boardColSize);
+int isAlive(int b, int c, int* board, int row, int col);	// cell helper
