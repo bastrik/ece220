55b3f7e1fc2f2a77fc65a558006d53d5ab6ca8b9
gao2 <36889473+gao2@users.noreply.github.com>
2017-03-13 00:54:28 +0000

    Committing grade and test files

--- MP6/updateBoard.h
+++ MP6/updateBoard.h
@@ -1,4 +1,3 @@
 void updateBoard(int* board, int boardRowSize, int boardColSize);
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col);
 int aliveStable(int* board, int boardRowSize, int boardColSize);
-int isAlive(int r, int c, int* board, int row, int col);	// cell helper
