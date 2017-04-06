e984a26759d5c2e7b8a8f4492e728f29d06e64f7
rzhang56 <rzhang56@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-04-06 15:03:51 +0000

    testing

--- LAB3/lab3.c
+++ LAB3/lab3.c
@@ -3,6 +3,22 @@
 #include <math.h>
 
 int main(){
-  // Start your code here
+  int arr1[5] = {1,2,3,4,5};
+
+  int arr2[5];
+
+  printf("int size: %lu, pointer size: %lu\n", sizeof(int), sizeof(int *));
+  int i;
+  for (i = 0; i < 5; i++)
+  {
+  	arr2[i] = &arr1[i];
+  }
+
+  for (i = 0; i < 5; i++)
+  {
+  	printf("%p, answer:%p\n", arr2[i], (arr1+i));
+  }
+
+  return 0;
 }
 
