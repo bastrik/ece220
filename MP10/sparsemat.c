95399a808fb3c9e49b790cf731213404eb309815
rzhang56 <rzhang56@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-04-12 03:58:24 +0000

    completed load and get val

--- MP10/sparsemat.c
+++ MP10/sparsemat.c
@@ -9,8 +9,32 @@
 
 sp_tuples * load_tuples(char* input_file)
 {
+	sp_tuples * newTuple = malloc(sizeof(sp_tuples));
 
-    return NULL;
+	FILE * file;
+	file = fopen(input_file, "r");   // read file, assume file is valid
+
+	int row, col;
+	double val;
+
+	fscanf(file, "%d", &row);
+    fscanf(file, "%d", &col);
+    // init tuples
+    newTuple->m = row;
+    newTuple->n = col;
+    newTuple->nz = 0;
+    newTuple->tuples_head = NULL;
+
+    while(feof(file) == 0)
+    {
+    	fscanf(file, "%d", &row);
+    	fscanf(file, "%d", &col);
+    	fscanf(file, "%lf", &val);
+    	set_tuples(newTuple, row, col, val);
+    }
+    fclose(file);
+
+    return newTuple;
 }
 
 
@@ -18,6 +42,18 @@ sp_tuples * load_tuples(char* input_file)
 double gv_tuples(sp_tuples * mat_t,int row,int col)
 
 {
+	int r, c;
+	sp_tuples_node * curr = mat_t->tuples_head;
+	// traverse linked list
+	while(curr != NULL)
+	{
+		r = curr->row;
+		c = curr->col;
+		if(r = row && c = col)
+			return curr->value;
+		curr = curr->next;
+	}
+
     return 0;
 }
 
